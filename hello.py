7a7888fe9cf5c6379cd1f2991bd8c64b10c8e773
SrikanthG25 <116871335+SrikanthG25@users.noreply.github.com>
2024-04-13 12:20:24 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -427,4 +427,23 @@ def Roundoff(n, decimals=0):
 #     address+=(", " + wrap_text[0])
 
 # print(address)
+
+
+# array search data element 
+def ser_data(arr , key):
+    for i, value in enumerate(arr):
+        if value == key:
+            return i
+    return -1
+arr_size = int(input())
+arr = []
+for i in range(arr_size):
+    num = int(input())
+    arr.append(num)
+key = int(input())
+result= ser_data(arr , key)
+if result != -1:
+    print("SUCCESSFUL")
+else:
+    print("UNSUCCESSFUL")
  
